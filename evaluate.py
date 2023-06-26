b36fd1b9f22e23c9b0ebd8699f24f113440b8357
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2023-06-26 13:14:01 +0200

    feat: implemented evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,9 @@
 import argparse
 from statistics import mean
+
+import torch
+from tqdm import tqdm
+
 from agents.random_agent import RandomAgent
 from agents.ai_agent import AIAgent
 from agents.q_agent import QAgent
@@ -13,7 +17,7 @@ def evaluate(game, agents, num_evaluations):
     total_wins = [0] * len(agents)
     points_history = [[] for _ in range(len(agents))]
 
-    for _ in range(num_evaluations):
+    for _ in tqdm(range(num_evaluations)):
         game_winner_id, winner_points, _ = brisc.play_episode(game, agents, train=False)
         for player in game.players:
             points_history[player.id].append(player.points)
@@ -22,46 +26,68 @@ def evaluate(game, agents, num_evaluations):
 
     print(f"\nTotal wins: {total_wins}.")
     for i in range(len(agents)):
-        print(f"{agents[i].name} {i} won {total_wins[i]/num_evaluations:.2%} with an average of {mean(points_history[i]):.2f} points.")
+        print(
+            f"{agents[i].name} {i} won {total_wins[i] / num_evaluations:.2%} with an average of {mean(points_history[i]):.2f} points.")
 
     return total_wins, points_history
 
 
-def main(argv=None):
+def main(args=None):
     """Evaluate agent performance against RandomAgent and AIAgent."""
     logger = BriscolaLogger(BriscolaLogger.LoggerLevels.TEST)
     game = brisc.BriscolaGame(2, logger)
 
     # agent to be evaluated is RandomAgent or QAgent if a model is provided
     if FLAGS.model_dir:
-        eval_agent = QAgent(network=FLAGS.network)
-        eval_agent.load_model(FLAGS.model_dir)
-        eval_agent.make_greedy()
+        print(f"Loading the model '{args.model_dir}'...")
+        checkpoint = torch.load(args.model_dir)
+        config = checkpoint['config']
+        agent = QAgent(
+            n_actions=config['n_actions'],
+            epsilon=config['epsilon'],
+            minimum_epsilon=config['minimum_epsilon'],
+            replay_memory_capacity=1000000,
+            minimum_training_samples=config['minimum_training_samples'],
+            batch_size=config['batch_size'],
+            discount=config['discount'],
+            loss_fn=config['loss_fn'],
+            learning_rate=0.0001,
+            replace_every=config['replace_every'],
+            epsilon_decay_rate=config['epsilon_decay_rate'],
+            layers=config['layers'],
+            state_type=config['state_type'],
+        )
+        agent.policy_net.load_state_dict(checkpoint['policy_state_dict'])
+        agent.make_greedy()
     else:
-        eval_agent = RandomAgent()
+        agent = RandomAgent()
 
     # test agent against RandomAgent
-    agents = [eval_agent, RandomAgent()]
+    print(f"Testing against RandomAgent on {args.num_evaluations} games")
+    agents = [agent, RandomAgent()]
     total_wins, points_history = evaluate(game, agents, FLAGS.num_evaluations)
     stats_plotter(agents, points_history, total_wins)
 
     # test agent against AIAgent
-    agents = [eval_agent, AIAgent()]
+    print(f"Testing against AIAgent on {args.num_evaluations} games")
+    agents = [agent, AIAgent()]
     total_wins, points_history = evaluate(game, agents, FLAGS.num_evaluations)
     stats_plotter(agents, points_history, total_wins)
 
 
 if __name__ == '__main__':
-
     # parameters
     # ==================================================
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--model_dir", default=None, help="Provide a trained model path if you want to play against a deep agent", type=str)
-    parser.add_argument("--network", default=NetworkTypes.DRQN, choices=[NetworkTypes.DQN, NetworkTypes.DRQN], help="Neural network used for approximating value function")
-    parser.add_argument("--num_evaluations", default=30, help="Number of evaluation games against each type of opponent for each test", type=int)
+    parser.add_argument("--model_dir", default="models/QLearningAgent_25k_66.7%_ruled.pt",
+                        help="Provide a trained model path if you want to play against a deep agent", type=str)
+    parser.add_argument("--network", default=NetworkTypes.DQN, choices=[NetworkTypes.DQN, NetworkTypes.DRQN],
+                        help="Neural network used for approximating value function")
+    parser.add_argument("--num_evaluations", default=10000,
+                        help="Number of evaluation games against each type of opponent for each test", type=int)
 
     FLAGS = parser.parse_args()
 
-    main()
+    main(FLAGS)
